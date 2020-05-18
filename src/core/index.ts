9848010cdd8258f3d4a68fbbc7abc9a141299d0b
Sasha Koss <koss@nocorp.me>
2020-05-18 09:00:49 +0400

    Make Path in route be more specific

--- src/core/index.ts
+++ src/core/index.ts
@@ -70,7 +70,7 @@ export const notFoundLocation: RouteNotFoundLocation<{}> = {
 
 export function route<
   RouteName extends string,
-  Path extends Function,
+  Path extends () => string,
   RouteMeta = {}
 >(
   name: RouteName,
